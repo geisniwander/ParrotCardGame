a632be08ba3f2112511e384f67fc294f5db5d5d4
geisniwander <geisniwander@gmail.com>
2022-10-22 16:11:39 -0300

    versao-funcional

--- js/script.js
+++ js/script.js
@@ -1,41 +1,43 @@
 
-let numCartas=0
+let numCartas=0;
+let jogadas=0;
+let finalizadas=0;
 const container = document.querySelector(".cartas");
 const cartas=[
-    `<div class="carta 1" onclick="virarCarta(this)">
-        <div class="face frente"><img src="./images/parrot.png"></div>
+    `<div class="carta 1">
+        <div class="face frente" onclick="virarCarta(this)"></div>
         <div class="face verso"><img src="./images/bobrossparrot.gif"></div>
     </div>`,
-    `<div class="carta 2" onclick="virarCarta(this)">
-        <div class="face frente"><img src="./images/parrot.png"></div>
+    `<div class="carta 2">
+        <div class="face frente" onclick="virarCarta(this)"></div>
         <div class="face verso"><img src="./images/explodyparrot.gif"></div>
     </div>`,
-    `<div class="carta 3" onclick="virarCarta(this)">
-        <div class="face frente"><img src="./images/parrot.png"></div>
+    `<div class="carta 3">
+        <div class="face frente" onclick="virarCarta(this)">></div>
         <div class="face verso"><img src="./images/fiestaparrot.gif"></div>
     </div>`,
-    `<div class="carta 4" onclick="virarCarta(this)">
-        <div class="face frente"><img src="./images/parrot.png"></div>
+    `<div class="carta 4">
+        <div class="face frente" onclick="virarCarta(this)"></div>
         <div class="face verso"><img src="./images/metalparrot.gif"></div>
     </div>`,
-    `<div class="carta 5" onclick="virarCarta(this)">
-        <div class="face frente"><img src="./images/parrot.png"></div>
+    `<div class="carta 5">
+        <div class="face frente" onclick="virarCarta(this)"></div>
         <div class="face verso"><img src="./images/revertitparrot.gif"></div>
     </div>`,
-    `<div class="carta 6" onclick="virarCarta(this)">
-        <div class="face frente"><img src="./images/parrot.png"></div>
+    `<div class="carta 6">
+        <div class="face frente" onclick="virarCarta(this)"></div>
         <div class="face verso"><img src="./images/tripletsparrot.gif"></div>
     </div>`,
-    `<div class="carta 7" onclick="virarCarta(this)">
-        <div class="face frente"><img src="./images/parrot.png"></div>
+    `<div class="carta 7">
+        <div class="face frente" onclick="virarCarta(this)"></div>
         <div class="face verso"><img src="./images/unicornparrot.gif"></div>
     </div>`
 ];
-perguntaQuantidade();
+setTimeout(perguntaQuantidade,500)
 
 function perguntaQuantidade(){
     numCartas = Number(prompt("Com quantas cartas quer jogar?"));
-    if(numCartas%2!==0){
+    if(numCartas%2!==0 || numCartas<4 || numCartas>14){
         perguntaQuantidade();
     }
     else{
@@ -67,26 +69,60 @@ function adicionaCartas(){
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-
-function virarCarta(item){
+let cont=0;
+function virarCarta(carta){
+    jogadas++;
+    cont++;
+    const item=carta.parentNode;
     const clicadaFrente = item.querySelector(".frente");
     const clicadaVerso = item.querySelector(".verso");
-    item.classList.add("virada");
-    clicadaFrente.classList.add('frente-vira');    
+    item.classList.add('virada');
     clicadaVerso.classList.add('verso-vira');
-    verificaIgualdade();
+    clicadaFrente.classList.add('frente-vira');    
+    if(cont==2){
+        cont=0;
+        setTimeout(verificaIgualdade,500);
+
+    }
+
 }
 
 function voltarCarta(c1,c2){
-    const c1Frente = item.querySelector(".frente");
-    const c1Verso = item.querySelector(".verso");
+    const c1Frente = c1.querySelector(".frente");
+    const c1Verso = c1.querySelector(".verso");
     c1.classList.remove("virada");
     c1Frente.classList.remove('frente-vira');    
     c1Verso.classList.remove('verso-vira');
     c2.classList.remove("virada");
-    const c2Frente = item.querySelector(".frente");
-    const c2Verso = item.querySelector(".verso");
+    const c2Frente = c2.querySelector(".frente");
+    const c2Verso = c2.querySelector(".verso");
     c2Frente.classList.remove('frente-vira');    
-    c2.classList.remove('verso-vira');
+    c2Verso.classList.remove('verso-vira');
 }
 
+function verificaIgualdade(){
+    const listaViradas=Array.from(document.querySelectorAll(".virada"));
+    let duasAtuais=[];
+
+    for(let i=0; i<=(listaViradas.length)-1; i++){
+        if(listaViradas[i].classList.contains("finalizada")==false){
+            duasAtuais.push(listaViradas[i]);
+        }
+        
+    }
+    for(let k=0; k<=duasAtuais.length; k++){
+        for(let j=1; j<=duasAtuais.length; j++){
+            if(duasAtuais[k].classList.value===duasAtuais[j].classList.value){
+                duasAtuais[k].classList.add("finalizada");
+                duasAtuais[j].classList.add("finalizada");
+                finalizadas++;
+                if(finalizadas==numCartas/2){
+                    alert(`Você ganhou em ${jogadas} jogadas!`);
+                }
+            }
+            else if(duasAtuais[k].classList.value!==duasAtuais[j].classList.value){
+                setTimeout(voltarCarta,500,duasAtuais[k],duasAtuais[j]);
+            }
+            }
+        }
+    }
